774e364c0c1f9f6ae6a52288b0c081a17a3f9f83
Ludovico <ludovico.francia@gmail.com>
2024-12-13 12:58:08 +0100

    fifth commit

--- main.py
+++ main.py
@@ -2,15 +2,16 @@ from utils import api_yes_no, disable_ssl_verification, display_menu, handle_bas
 
 def main():
   print("\n--- --- Welcome to Flyback! --- --- \n")
-  amadeus_client= api_yes_no()
   disable_ssl_verification()
 
   # Main program loop
   while True:
       choice= display_menu()
       if choice == "1":
+          amadeus_client= api_yes_no()
           handle_basic_search(amadeus_client)
       elif choice == "2":
+          amadeus_client= api_yes_no()
           handle_weekdays_search(amadeus_client)
       elif choice == "3":
           print("\n Thank you for using FlyBack! \n")
